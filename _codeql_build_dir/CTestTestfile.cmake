fdcaf3d944bfe479b71b13f05a9fd6c99f14c544
Sjneugent <Sjneugent@users.noreply.github.com>
2025-12-03 13:48:21 +0000

    removed the build dir stuff

--- _codeql_build_dir/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/runner/work/CppCurses/CppCurses
-# Build directory: /home/runner/work/CppCurses/CppCurses/_codeql_build_dir
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/ftxui-build")
-subdirs("_deps/googletest-build")
-subdirs("tests")
